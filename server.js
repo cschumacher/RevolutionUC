6e55eea6e77d57df0c64afc828be419582835d8e
Curt Schumacher <schumacr@mail.uc.edu>
2014-07-20 15:26:29 -0400

    Remove index.html and signup.html

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 
 var express = require('express'),
 		app = express(),
-		port = 3000;
+		port = 3000,
+		path = require('path');
 
 require('./app/routes')(app);
 
 app.set('views', './app/views');
 app.set('view engine', 'jade');
 app.set('view options', {doctype: 'html'});
+app.use(require('serve-static')(path.join(rootPath, 'public')));
 
 
 var server = app.listen(port, function() {
